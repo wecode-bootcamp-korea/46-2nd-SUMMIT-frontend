cca3c85cb948242ef7c16b746d9e2c4ed0980187
GUNWOONG JANG <gunwoong32@gmail.com>
2023-06-16 19:09:11 +0900

    Add: 초기 세팅 완료

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -7,6 +7,9 @@ const GlobalStyle = createGlobalStyle`
 
   * {
     box-sizing: border-box;
+     font-family: 'Noto Sans KR', sans-serif;
+  }
+  body{
     font-family: 'Noto Sans KR', sans-serif;
   }
 `;
