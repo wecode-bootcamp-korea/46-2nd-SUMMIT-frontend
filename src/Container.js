dc82f1932c95add920f38a1cd4fbc55a76acca39
GUNWOONG JANG <gunwoong32@gmail.com>
2023-06-17 12:36:28 +0900

    Add: Outlet 추가

---
+++ src/Container.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Nav from './components/Nav/Nav.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+const Container = () => {
+  return (
+    <>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default Container;
