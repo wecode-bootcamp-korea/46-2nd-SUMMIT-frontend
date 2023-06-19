20c4745959996e13ea004e695205000eb0a31fbf
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-06-19 12:19:36 +0900

    Add: Login 페이지 레이아웃 추가

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {
+  LoginPage,
+  LoginContainer,
+  MainTitle,
+  Description,
+  LoginLinkBtn,
+} from './Login';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <LoginPage>
+      <LoginContainer>
+        <MainTitle>로그인</MainTitle>
+        <Description>Search Ur Musical in SUM</Description>
+        <LoginLinkBtn>카카오톡 간편 로그인</LoginLinkBtn>
+      </LoginContainer>
+    </LoginPage>
+  );
 };
 
 export default Login;
