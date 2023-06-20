46793382b79120cf109ada3965bb3ff48f2ee1ae
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-06-20 17:34:04 +0900

    Add: login 페이지 fetch 코드 추가

--- src/pages/Login/Oauth/Oauth.jsx
+++ src/pages/Login/Oauth/Oauth.jsx
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { APIS } from '../../../config.js';
+import { LoginPage } from '../Login';
 
 const Oauth = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const code = new URL(window.location.href).searchParams.get('code');
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_URI = 'http://localhost:3000/oauth';
+
+  useEffect(() => {
+    fetch(
+      `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        if (data.access_token) {
+          fetch(APIS.login, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json;charset=utf-8',
+              Authorization: data.access_token,
+            },
+          })
+            .then(res => res.json())
+            .then(res => {
+              if (res.token) {
+                localStorage.setItem('token', res.token);
+                navigate('/');
+              }
+            });
+        }
+      });
+  }, []);
+
+  return <LoginPage />;
 };
 
 export default Oauth;
