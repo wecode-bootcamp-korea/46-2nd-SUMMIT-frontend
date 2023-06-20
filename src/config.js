070932819eb3b99be8aca98872d74dc0dcd51778
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-06-20 17:34:04 +0900

    Add: login 페이지 fetch 코드 추가

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const BASE_URL_K = 'http://10.58.52.71:8001';
+export const BASE_URL_L = 'http://10.58.52.105:3000';
+
+export const APIS = {
+  login: `${BASE_URL_L}/users/kakao-login`,
+};
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import {
   LoginPage,
   LoginContainer,
@@ -8,12 +9,18 @@ import {
 } from './Login';
 
 const Login = () => {
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_URI = 'http://localhost:3000/oauth';
+  const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
   return (
     <LoginPage>
       <LoginContainer>
         <MainTitle>로그인</MainTitle>
         <Description>Search Ur Musical in SUM</Description>
-        <LoginLinkBtn>카카오톡 간편 로그인</LoginLinkBtn>
+        <a href={KAKAO_AUTH_URI}>
+          <LoginLinkBtn>카카오톡 간편 로그인</LoginLinkBtn>
+        </a>
       </LoginContainer>
     </LoginPage>
   );
