eb47300bed837edcc7a679fcfacc062c01d4d8c2
GUNWOONG JANG <gunwoong32@gmail.com>
2023-06-27 14:50:53 +0900

    Add: 페이지네이션 추가

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-export const BASE_URL_K = 'http://10.58.52.117:5000';
-export const BASE_URL_L = 'http://10.58.52.117:8071';
+export const BASE_URL_K = 'http://10.58.52.50:8071';
+export const BASE_URL_L = 'http://10.58.52.66:8080';
 
 export const APIS = {
-  login: `${BASE_URL_L}/users/kakao-login`,
+  login: `${BASE_URL_K}/users/kakao-login`,
   wish: `${BASE_URL_K}/wishs`,
   register: `${BASE_URL_L}/showposts/create`,
   showList: `${BASE_URL_K}/shows`,
