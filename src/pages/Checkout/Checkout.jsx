a336716aebf34e82a80fb70e4eb328db25837351
zosuzin <zosuzin@gmail.com>
2023-06-28 00:04:15 +0900

    [46기 조수진] ADD: 결제페이지 recoil과 fetch통신기능 추가

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,18 +1,121 @@
-import React, { useState } from 'react';
-import * as S from './Checkout';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Bootpay } from '@bootpay/client-js';
+import { APIS } from '../../config.js';
+import { useRecoilState } from 'recoil';
+import checkoutInfoState from '../../atom.js';
 import { Subject } from './Subject.jsx';
-import { Payment } from './Payment.jsx';
+import Modal from '../../components/Modal/Modal.jsx';
 import TermList from './TermList.jsx';
-import Button from '../../components/Button/Button.jsx';
+import * as S from './Checkout.js';
 
 const Checkout = () => {
+  const navigate = useNavigate();
   const [checked, setChecked] = useState('');
   const [point, setPoint] = useState(0);
+  const [userPoint, setUserPoint] = useState(0);
   const [isClickUseBtn, setIsClickUseBtn] = useState(false);
-  const currentPoint = isClickUseBtn ? 100000 - point : 100000;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [sucModalOpen, setSucModalOpen] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const checkoutInfo = useRecoilState(checkoutInfoState);
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    let receivePrice = 0;
+    if (checkoutInfo[0].totalPrice !== undefined) {
+      receivePrice = checkoutInfo[0].totalPrice;
+    }
+
+    if (isClickUseBtn) {
+      setTotalPrice(receivePrice - point + 1000);
+    } else {
+      setTotalPrice(receivePrice + 1000);
+    }
+  }, [isClickUseBtn]);
+
+  useEffect(() => {
+    fetch(`${APIS.getPoint}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: token,
+      },
+    })
+      .then(response => response.json())
+      .then(response => {
+        setUserPoint(Math.floor(response.user[0].point));
+      });
+  }, []);
 
   const handleCheck = checkBoxNum => {
     setChecked(checkBoxNum);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSucModalOpen(prevState => {
+      if (prevState) {
+        navigate('/');
+      }
+      return false;
+    });
+  };
+
+  const payHandler = async () => {
+    try {
+      const response = await Bootpay.requestPayment({
+        application_id: process.env.REACT_APP_PAY_APPLICATION_ID,
+        price: totalPrice,
+        order_name: '조수진',
+        order_id: 'TEST_ORDER_ID',
+        pg: '카카오페이',
+        method: '간편',
+        tax_free: 0,
+        user: {
+          id: '회원아이디',
+          username: '회원이름',
+          phone: '01000000000',
+          email: 'test@test.com',
+        },
+        items: [
+          {
+            id: 'item_id',
+            name: '테스트아이템',
+            qty: 1,
+            price: totalPrice,
+          },
+        ],
+        extra: {
+          open_type: 'iframe',
+          card_quota: '0,2,3',
+          escrow: false,
+        },
+      });
+      if (response.event === 'done') {
+        fetch(`${APIS.checkout}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            Authorization: token,
+          },
+          body: JSON.stringify({
+            seatsId: 28,
+            quantity: 2,
+            point: point,
+            totalPrice: totalPrice,
+          }),
+        })
+          .then(res => {
+            return res.json();
+          })
+          .then(res => {
+            setSucModalOpen(true);
+          });
+      }
+    } catch (error) {
+      setIsModalOpen(true);
+    }
   };
 
   return (
@@ -54,19 +157,51 @@ const Checkout = () => {
         >
           사용
         </S.AgreeBtn>
-        <S.InfTitle> (잔여포인트: {currentPoint}P)</S.InfTitle>
+        <S.InfTitle>
+          {`(잔여포인트: ${isClickUseBtn ? userPoint - point : userPoint}P)`}
+        </S.InfTitle>
       </S.InfBox>
       <S.InfBox>
         <S.CheckBox />
         <S.InfTitle>카카오페이</S.InfTitle>
       </S.InfBox>
       <S.Title>결제금액</S.Title>
-      <Payment point={point} isClickUseBtn={isClickUseBtn} />
+
+      <S.InfBox>
+        <S.InfTitle title="first">티켓금액</S.InfTitle>
+        <S.InfTitle>{checkoutInfo[0].totalPrice}</S.InfTitle>
+      </S.InfBox>
+      <S.InfBox>
+        <S.InfTitle title="first">포인트사용</S.InfTitle>
+        <S.InfTitle>- {isClickUseBtn ? point : 0} 원</S.InfTitle>
+      </S.InfBox>
+      <S.InfBox>
+        <S.InfTitle title="first">수수료</S.InfTitle>
+        <S.InfTitle>1000 원</S.InfTitle>
+      </S.InfBox>
+      <S.InfBox>
+        <S.InfTitle title="first">총결제금액</S.InfTitle>
+        <S.InfTitle>{totalPrice} 원</S.InfTitle>
+      </S.InfBox>
       <S.Title>이용동의</S.Title>
       <TermList />
       <S.Flex>
-        <Button size="medium" text="결제하기" />
+        <S.Button onClick={payHandler}>결제하기</S.Button>
       </S.Flex>
+      {isModalOpen && (
+        <Modal
+          title="결제 실패"
+          message="다시 시도해주세요."
+          onClose={closeModal}
+        />
+      )}
+      {sucModalOpen && (
+        <Modal
+          title="결제 성공"
+          message="등록하신 메일로 예약정보 전송됩니다."
+          onClose={closeModal}
+        />
+      )}
     </S.Page>
   );
 };
--- src/pages/Checkout/Subject.jsx
+++ src/pages/Checkout/Subject.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import * as S from './Checkout';
+import { useRecoilState } from 'recoil';
+import checkoutInfoState from '../../atom.js';
+import { ReservationAtom } from '../../Recoil/ReservationAtom.jsx';
 
 export const Subject = () => {
+  const checkoutInfo = useRecoilState(checkoutInfoState);
+  const reservationData = useRecoilState(ReservationAtom);
+
   const SUBJECT = [
-    { title: '공연명', content: '레미제라블' },
-    { title: '예매날짜', content: '2023-06-01' },
-    { title: '예매시간', content: '17:00' },
-    { title: '좌석등급', content: 'R석' },
-    { title: '좌석가격', content: '80,000원' },
+    { title: '공연명', content: checkoutInfo[0].title },
+    { title: '예매날짜', content: reservationData[0].date },
+    { title: '예매시간', content: reservationData[0].time },
+    {
+      title: '예매좌석',
+      content:
+        checkoutInfo[0].grade === 'VIP석'
+          ? `${checkoutInfo[0].selectedTicket}석`
+          : checkoutInfo[0].grade === 'R석'
+          ? `${checkoutInfo[0].selectedTicket}석`
+          : checkoutInfo[0].grade === 'S석'
+          ? `${checkoutInfo[0].selectedTicket}석`
+          : '0석',
+    },
+    {
+      title: '좌석가격',
+      content:
+        checkoutInfo[0].totalPrice !== undefined
+          ? `${checkoutInfo[0].totalPrice.toLocaleString()}원`
+          : '0원',
+    },
   ];
   return (
     <>
