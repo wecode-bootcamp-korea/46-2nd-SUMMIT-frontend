2dd4761f8fcf2298c82e328844923cc80bd3afa6
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-06-27 13:54:04 +0900

    Add: Nav-wishlist UI 추가

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL_K = 'http://10.58.52.117:8071';
-export const BASE_URL_L = 'http://10.58.52.154:8071';
+export const BASE_URL_L = 'http://10.58.52.147:8080';
 
 export const APIS = {
   login: `${BASE_URL_L}/users/kakao-login`,
